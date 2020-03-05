5f98c5156388e179f50c3a48a7a19206fe336401
StanleyHuang <infamous_godhand@yahoo.com>
2020-03-05 15:39:10 +1100

    getting post data for /addhack

--- addHack.js
+++ addHack.js
@@ -1,4 +1,14 @@
+const mongoose = require("mongoose");
+
+let conn = null;
+
+const uri = `mongodb://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@ds157136.mlab.com:57136/hackone`;
+
 export const add = async (event, context) => {
+  context.callbackWaitsForEmptyEventLoop = false;
+  const data = JSON.parse(event.body);
+  const { title, description } = data;
+
   return {
     statusCode: 200,
     body: JSON.stringify({
