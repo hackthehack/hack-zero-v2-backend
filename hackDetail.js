998457bb5a80c302f2920a6634e11b6b1de3370b
StanleyHuang <infamous_godhand@yahoo.com>
2020-03-06 14:15:48 +1100

    setting up route for hack detail

---
+++ hackDetail.js
@@ -0,0 +1,12 @@
+const mongoose = require("mongoose");
+// const HackModel = require("./model/hack");
+
+let conn = null;
+const url = `mongodb://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@ds157136.mlab.com:57136/hackone`;
+
+export const detail = async (event, context) => {
+  return {
+    statusCode: 200,
+    body: "this is detail route"
+  };
+};
