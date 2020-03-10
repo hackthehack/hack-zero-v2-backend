c9833eb5bc293057bc069ea151f9263ea423b0ae
StanleyHuang <infamous_godhand@yahoo.com>
2020-03-11 09:35:58 +1100

    able to use util.promisify in registerUser lambda

--- registerUser.js
+++ registerUser.js
@@ -1,5 +1,5 @@
 let AWS = require("aws-sdk");
-
+const util = require("util");
 AWS.config.update({
   accessKeyId: process.env.AWS_ACC_KEY_ID,
   secretAccessKey: process.env.AWS_SECRET
@@ -8,35 +8,21 @@ const COGNITO_CLIENT = new AWS.CognitoIdentityServiceProvider({
   apiVersion: "2016-04-19",
   region: "us-east-1"
 });
-const setPasswordPromise = userParams => {
-  return new Promise((resolve, reject) => {
-    COGNITO_CLIENT.adminSetUserPassword(userParams, (error, data) => {
-      if (error) {
-        //console.log(error);
-        reject("unable to set password");
-      }
-      resolve(data);
-    });
-  });
-};
-const createUserPromise = userParams => {
-  return new Promise((resolve, reject) => {
-    COGNITO_CLIENT.adminCreateUser(userParams, (error, data) => {
-      if (error) {
-        //console.log(error);
-        reject("Unable to create user");
-      }
-      //console.log(data);
-      resolve(data);
-    });
-  });
-};
+
+const utilPromiseRegisterUser = util
+  .promisify(COGNITO_CLIENT.adminCreateUser)
+  .bind(COGNITO_CLIENT);
+
+const utilPromiseSetPassword = util
+  .promisify(COGNITO_CLIENT.adminSetUserPassword)
+  .bind(COGNITO_CLIENT);
 
 export const register = async (event, context) => {
   // console.log(event.body);
   const data = JSON.parse(event.body);
   const { email, password } = data;
   // let awsUserName = "";
+
   const createUserParams = {
     UserPoolId: process.env.AWS_USER_POOL_ID,
     Username: email,
@@ -51,19 +37,14 @@ export const register = async (event, context) => {
     Username: email /* required */,
     Permanent: true
   };
-  await createUserPromise(createUserParams);
-  //console.log(result);
-  // awsUserName = result.User.Username;
-  //console.log("aws username");
-  //console.log(awsUserName);
-  await setPasswordPromise(passwordParams);
-  //console.log(passwordResult);
+  await utilPromiseRegisterUser(createUserParams);
+  await utilPromiseSetPassword(passwordParams);
 
   return {
     statusCode: 200,
     headers: {
-      'Access-Control-Allow-Origin': '*',
-      'Access-Control-Allow-Credentials': true,
+      "Access-Control-Allow-Origin": "*",
+      "Access-Control-Allow-Credentials": true
     },
     body: "User created"
   };
