c5d10c5b76e0a830900c30396cf38ae36fb9794a
StanleyHuang <infamous_godhand@yahoo.com>
2020-03-10 13:13:12 +1100

    setting up to test register user

--- registerUser.js
+++ registerUser.js
@@ -1,1 +1,18 @@
-export const registerUser = (event, context) => {};
+let AWS = require("aws-sdk");
+AWS.config.update({
+  accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+  secretAccessKey: process.env.AWS_SECRET
+});
+const COGNITO_CLIENT = new AWS.CognitoIdentityServiceProvider({
+  apiVersion: "2016-04-19",
+  region: "us-east-1"
+});
+export const register = async (event, context) => {
+  // console.log("register route");
+  // console.log(process.env.AWS_SECRET);
+  // console.log(process.env.AWS_ACCESS_KEY_ID);
+  return {
+    statusCode: 200,
+    body: "register route"
+  };
+};
