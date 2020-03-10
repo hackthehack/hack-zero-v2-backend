dd2a5b419f8a05f56aaf94ba426236ce5e2aa06b
StanleyHuang <infamous_godhand@yahoo.com>
2020-03-10 13:53:39 +1100

    promisify the createUser function

--- registerUser.js
+++ registerUser.js
@@ -1,4 +1,5 @@
 let AWS = require("aws-sdk");
+let util = require("util");
 AWS.config.update({
   accessKeyId: process.env.AWS_ACCESS_KEY_ID,
   secretAccessKey: process.env.AWS_SECRET
@@ -7,10 +8,29 @@ const COGNITO_CLIENT = new AWS.CognitoIdentityServiceProvider({
   apiVersion: "2016-04-19",
   region: "us-east-1"
 });
+const createUserPromise = userParams => {
+  return new Promise((resolve, reject) => {
+    COGNITO_CLIENT.adminCreateUser(userParams, (error, data) => {
+      if (error) {
+        console.log(error);
+        reject("Unable to create user");
+      }
+      resolve(data);
+    });
+  });
+};
+
 export const register = async (event, context) => {
-  // console.log("register route");
-  // console.log(process.env.AWS_SECRET);
-  // console.log(process.env.AWS_ACCESS_KEY_ID);
+  const params = {
+    UserPoolId: process.env.AWS_USER_POOL_ID,
+    Username: "test3@test.com",
+    DesiredDeliveryMediums: ["EMAIL"],
+    ForceAliasCreation: false,
+    TemporaryPassword: "Test@1234",
+    UserAttributes: [{ Name: "email", Value: "test3@test.com" }]
+  };
+  let result = await createUserPromise(params);
+  console.log(result);
   return {
     statusCode: 200,
     body: "register route"
