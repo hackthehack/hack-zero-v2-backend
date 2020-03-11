fb3cbb48519e3ed0fc6ac8954635b95e20957bb4
Joseph Garner <josephgarner@Josephs-MacBook-Pro.local>
2020-03-12 10:38:15 +1100

    Inital setup TODO

---
+++ joinHack.js
@@ -0,0 +1,53 @@
+const mongoose = require("mongoose");
+
+let conn = null;
+
+const uri = `mongodb://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@ds157136.mlab.com:57136/hackone`;
+
+export const join = async (event, context) => {
+  context.callbackWaitsForEmptyEventLoop = false;
+  const data = JSON.parse(event.body);
+  const { title, description, goal } = data;
+
+  if (conn == null) {
+    conn = await mongoose.createConnection(uri, {
+      // Buffering means mongoose will queue up operations if it gets
+      // disconnected from MongoDB and send them when it reconnects.
+      // With serverless, better to fail fast if not connected.
+      bufferCommands: false, // Disable mongoose buffering
+      bufferMaxEntries: 0 // and MongoDB driver buffering
+    });
+    conn.model(
+      "Hack",
+      new mongoose.Schema({ title: String, description: String, goal: String })
+    );
+  }
+  const Hack = conn.model("Hack");
+
+  try {
+    const newHack = new Hack({ title, description, goal });
+    await newHack.save();
+
+    return {
+      statusCode: 200,
+      headers: {
+        "Access-Control-Allow-Origin": process.env.ACCESS_CONTROL_ALLOW_ORIGIN,
+        "Access-Control-Allow-Credentials": true
+      },
+      body: JSON.stringify({
+        message: `hack created`
+      })
+    };
+  } catch (err) {
+    return {
+      statusCode: 500,
+      headers: {
+        "Access-Control-Allow-Origin": process.env.ACCESS_CONTROL_ALLOW_ORIGIN,
+        "Access-Control-Allow-Credentials": true
+      },
+      body: JSON.stringify({
+        message: `Unable to create hack`
+      })
+    };
+  }
+};
