470c496e2c4a6acf8e2227cff214976db5ff5813
StanleyHuang <infamous_godhand@yahoo.com>
2020-03-17 16:30:45 +1100

    more set up

--- tests/getHacks.test.js
+++ tests/getHacks.test.js
@@ -1,1 +1,10 @@
 import { list } from "../getHacks";
+import mongoose from "mongoose";
+
+const hackSchema = new mongoose.Schema({
+  title: String,
+  description: String,
+  goal: String
+});
+
+const Hack = mongoose.model("Hack", hackSchema);
