564a76766864d3997d9f6223de52f7ebb0136be8
StanleyHuang <infamous_godhand@yahoo.com>
2020-03-19 10:40:03 +1100

    added sudo do script

--- .github/scripts/decrypt.sh
+++ .github/scripts/decrypt.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 # Decrypt the file
-mkdir $Home/secrets
+sudo mkdir $Home/secrets
 # --batch to prevent interactive command --yes to assume "yes" for questions
-gpg --quiet --batch --yes --decrypt --passphrase="$SECRET_PASSPHRASE" \
+sudo gpg --quiet --batch --yes --decrypt --passphrase="$SECRET_PASSPHRASE" \
 --output $Home/secrets/.env.production .env.gpg
