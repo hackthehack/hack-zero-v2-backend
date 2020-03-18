3910778b5f17c7c03bb0f9275d45844cfcc4f406
StanleyHuang <infamous_godhand@yahoo.com>
2020-03-19 10:56:33 +1100

    try print out secret in decrypt script

--- .github/scripts/decrypt.sh
+++ .github/scripts/decrypt.sh
@@ -5,3 +5,5 @@ sudo mkdir $Home/secrets
 # --batch to prevent interactive command --yes to assume "yes" for questions
 sudo gpg --quiet --batch --yes --decrypt --passphrase="$SECRET_PASSPHRASE" \
 --output $Home/secrets/.env.production .env.gpg
+
+cat $Home/secrets/.env.production
