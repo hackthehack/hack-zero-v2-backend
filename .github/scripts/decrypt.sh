86154841efd874202292dc01efa02d84435638b9
StanleyHuang <infamous_godhand@yahoo.com>
2020-03-19 10:16:45 +1100

    serverless not able to find the decrypted file, added /Users/stanleyhunag in decrypt.sh

--- .github/scripts/decrypt.sh
+++ .github/scripts/decrypt.sh
@@ -4,4 +4,4 @@
 
 # --batch to prevent interactive command --yes to assume "yes" for questions
 gpg --quiet --batch --yes --decrypt --passphrase="$SECRET_PASSPHRASE" \
---output .env.production .env.gpg
+--output $HOME/.env.production .env.gpg
