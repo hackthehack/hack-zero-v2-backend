a6f04893566ffe89a8c8e066d0ea3cf7a647de38
StanleyHuang <infamous_godhand@yahoo.com>
2020-03-19 10:59:50 +1100

    testing if we can create the file in root

--- .github/scripts/decrypt.sh
+++ .github/scripts/decrypt.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 
 # Decrypt the file
-sudo mkdir $Home/secrets
+#sudo mkdir $Home/secrets
 # --batch to prevent interactive command --yes to assume "yes" for questions
 sudo gpg --quiet --batch --yes --decrypt --passphrase="$SECRET_PASSPHRASE" \
---output $Home/secrets/.env.production .env.gpg
+--output .env.production .env.gpg
 
-cat $Home/secrets/.env.production
+cat .env.production
