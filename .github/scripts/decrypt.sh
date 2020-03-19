f2506c528c473cb0d160215b1deabfd3086a44e3
StanleyHuang <infamous_godhand@yahoo.com>
2020-03-19 14:33:53 +1100

    trying out to see if we can access github/home local file system

--- .github/scripts/decrypt.sh
+++ .github/scripts/decrypt.sh
@@ -4,6 +4,7 @@
 #sudo mkdir $Home/secrets
 # --batch to prevent interactive command --yes to assume "yes" for questions
 sudo gpg --quiet --batch --yes --decrypt --passphrase="$SECRET_PASSPHRASE" \
---output .env .env.gpg
+--output $HOME/.env.production .env.gpg
 
+ls $HOME/
 ls -a
