4e278f7b6b245c803c77df5b3ce3ce504145712a
StanleyHuang <infamous_godhand@yahoo.com>
2020-03-19 14:48:33 +1100

    forgot to mkdir secret folder

--- .github/scripts/decrypt.sh
+++ .github/scripts/decrypt.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 
 # Decrypt the file
-#sudo mkdir $Home/secrets
+sudo mkdir $HOME/secrets
 # --batch to prevent interactive command --yes to assume "yes" for questions
 sudo gpg --quiet --batch --yes --decrypt --passphrase="$SECRET_PASSPHRASE" \
 --output $HOME/secrets/.env.production .env.gpg
 
-ls $HOME/
+ls $HOME
 ls -a
