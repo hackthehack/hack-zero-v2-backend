01baae64fa27ca1a0c09986fea64e5f542075ac7
StanleyHuang <infamous_godhand@yahoo.com>
2020-03-19 14:53:57 +1100

    checking what is in /Users/stanleyhunag

--- .github/scripts/decrypt.sh
+++ .github/scripts/decrypt.sh
@@ -1,10 +1,11 @@
 #!/bin/sh
 
 # Decrypt the file
-sudo mkdir $HOME/secrets
+# sudo mkdir $HOME/secrets
 # --batch to prevent interactive command --yes to assume "yes" for questions
 sudo gpg --quiet --batch --yes --decrypt --passphrase="$SECRET_PASSPHRASE" \
---output $HOME/secrets/.env.production .env.gpg
+--output .env.production .env.gpg
 
 ls $HOME
+echo "++++++++++++"
 ls -a
