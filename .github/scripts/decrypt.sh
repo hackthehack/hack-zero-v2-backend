f7a33b246265c48e092a838b77585650ab863d44
StanleyHuang <infamous_godhand@yahoo.com>
2020-03-19 16:08:08 +1100

    amended dotenv path yet again....

--- .github/scripts/decrypt.sh
+++ .github/scripts/decrypt.sh
@@ -1,13 +1,13 @@
 #!/bin/sh
 
 # Decrypt the file
-#sudo mkdir $HOME/secrets
+sudo mkdir $HOME/secrets
 # --batch to prevent interactive command --yes to assume "yes" for questions
 sudo gpg --quiet --batch --yes --decrypt --passphrase="$SECRET_PASSPHRASE" \
---output .env.production .env.gpg
+--output $HOME/secrets/.env.production .env.gpg
 
 # ls $HOME/
 # echo "-----ROOT-----Above----"
-echo "----current WORKDIR-----"
-ls -a /home/runner/work/hack-zero-v2-backend/hack-zero-v2-backend
-echo "++++++++++++"
+# echo "----current WORKDIR-----"
+# ls -a /home/runner/work/hack-zero-v2-backend/hack-zero-v2-backend
+# echo "++++++++++++"
