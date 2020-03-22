a391f6c85f9a772c178981f2bde39f0bf496a7ae
StanleyHuang <infamous_godhand@yahoo.com>
2020-03-23 09:24:08 +1100

    added authorizer for /addhack and /edithack

--- checkAuth.js
+++ checkAuth.js
@@ -1,1 +1,25 @@
-export const auth = async (event, context) => {};
+import jwt from "jsonwebtoken";
+import axios from "axios";
+import jwkToPem from "jwk-to-pem";
+
+const generatePolicy = (principalId, effect, resource) => {
+  const authResponse = {};
+  authResponse.principalId = principalId;
+  if (effect && resource) {
+    const policyDocument = {};
+    policyDocument.Version = "2012-10-17";
+    policyDocument.Statement = [];
+    const statementOne = {};
+    statementOne.Action = "execute-api:Invoke";
+    statementOne.Effect = effect;
+    statementOne.Resource = resource;
+    policyDocument.Statement[0] = statementOne;
+    authResponse.policyDocument = policyDocument;
+  }
+  return authResponse;
+};
+export const auth = async (event, context) => {
+  const token = event.authorizationToken;
+
+  if (!token) return "Not authorized, no token!!";
+};
