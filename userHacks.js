6cee9ee4802154d5f141ca2deaa2f701a2cc154e
Joseph Garner <josephgarner@Josephs-MacBook-Pro.local>
2020-03-24 14:48:30 +1100

    Dashboard: Display User assigned hacks
    Added userlist endpoint

---
+++ userHacks.js
@@ -0,0 +1,46 @@
+const mongoose = require("mongoose");
+// const HackModel = require("./model/hack");
+
+let conn = null;
+const url = `mongodb://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@ds157136.mlab.com:57136/hackone`;
+
+/**
+ * Lists all Hacks currently stored in the database
+ * @param {*} event
+ * @param {*} context
+ */
+export const list = async (event, context) => {
+  const userid = event.pathParameters.id;
+  context.callbackWaitsForEmptyEventLoop = false;
+  if (conn == null) {
+    conn = await mongoose.createConnection(url, {
+      bufferCommands: false,
+      bufferMaxEntries: 0
+    });
+    conn.model(
+      "Hack",
+      new mongoose.Schema({ title: String, description: String, team: Array })
+    );
+  }
+  const Query = conn.model("Hack");
+  try {
+    const doc = await Query.find( { "team": mongoose.Types.ObjectId(userid) } ).select('_id title description');
+    return {
+      statusCode: 200,
+      headers: {
+        "Access-Control-Allow-Origin": process.env.ACCESS_CONTROL_ALLOW_ORIGIN,
+        "Access-Control-Allow-Credentials": true
+      },
+      body: JSON.stringify(doc)
+    };
+  } catch (err) {
+    return {
+      statusCode: 500,
+      headers: {
+        "Access-Control-Allow-Origin": process.env.ACCESS_CONTROL_ALLOW_ORIGIN,
+        "Access-Control-Allow-Credentials": true
+      },
+      body: err
+    };
+  }
+};
