5ae48d05cde76737c32d79cd07b6e073b7d55b2b
StanleyHuang <infamous_godhand@yahoo.com>
2020-03-27 13:54:01 +1100

    need to actually find out how to run test in a CI

---
+++ tests/protectedRoutes.test.js
@@ -0,0 +1,9 @@
+import request from "supertest";
+
+test("/edithack route should not be accessible without jwt token", async done => {
+  // const response = await request("http://localhost:3001/edithack")
+  //   .post()
+  //   .send({ data: "test" });
+  // console.log(response);
+  done();
+});
