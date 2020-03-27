e55a4a01f2884e226da894d8aabcaa1bb4cc8312
StanleyHuang <infamous_godhand@yahoo.com>
2020-03-27 14:25:42 +1100

    first test working for lambda

--- tests/protectedRoutes.test.js
+++ tests/protectedRoutes.test.js
@@ -1,9 +1,9 @@
 import request from "supertest";
 
 test("/edithack route should not be accessible without jwt token", async done => {
-  // const response = await request("http://localhost:3001/edithack")
-  //   .post()
-  //   .send({ data: "test" });
-  // console.log(response);
+  const response = await request("http://localhost:3001")
+    .post("/edithack")
+    .send({ data: "test" });
+  expect(response.statusCode).toBe(401);
   done();
 });
