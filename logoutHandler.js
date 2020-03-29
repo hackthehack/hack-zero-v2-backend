11d28910c7c45ddb94a0141c512efa6ab48d89df
StanleyHuang <infamous_godhand@yahoo.com>
2020-03-30 10:08:46 +1100

    setting up function for admin logout

--- logoutHandler.js
+++ logoutHandler.js
@@ -1,1 +1,16 @@
+import AWS from "aws-sdk";
+import util from "util";
+const { promisify } = util;
+
+AWS.config.update({
+  accessKeyId: process.env.AWS_ACC_KEY_ID,
+  secretAccessKey: process.env.AWS_SECRET
+});
+const COGNITO_CLIENT = new AWS.CognitoIdentityServiceProvider({
+  apiVersion: "2016-04-19",
+  region: "us-east-1"
+});
+const utilPromiseAdminLogout = promisify(
+  COGNITO_CLIENT.adminUserGlobalSignOut
+).bind(COGNITO_CLIENT);
 export const logout = async (event, context) => {};
