2fc97b7246e03113a57237dc152f61b4234f150a
StanleyHuang <infamous_godhand@yahoo.com>
2020-04-03 10:01:09 +1100

    able to test invalid token AFTER user has been logged out

--- checkAuth.js
+++ checkAuth.js
@@ -1,6 +1,20 @@
 import jwt from "jsonwebtoken";
 import axios from "axios";
 import jwkToPem from "jwk-to-pem";
+import AWS from "aws-sdk";
+import util from "util";
+
+AWS.config.update({
+  accessKeyId: process.env.AWS_ACC_KEY_ID,
+  secretAccessKey: process.env.AWS_SECRET
+});
+const COGNITO_CLIENT = new AWS.CognitoIdentityServiceProvider({
+  apiVersion: "2016-04-19",
+  region: "us-east-1"
+});
+const utilPromiseGetUser = util
+  .promisify(COGNITO_CLIENT.getUser)
+  .bind(COGNITO_CLIENT);
 
 const generatePolicy = (principalId, effect, resource) => {
   const authResponse = {};
@@ -32,7 +46,7 @@ export const auth = async (event, context) => {
 
   try {
     const decoded = jwt.verify(tokenValue, pem, { algorithm: ["RS256"] });
-
+    await utilPromiseGetUser({ AccessToken: tokenValue });
     return generatePolicy(decoded.sub, "Allow", event.methodArn);
   } catch (err) {
     return new Error("Not authorized");
