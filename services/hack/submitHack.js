d420046129d9b8c21e285a530d31151bc2280970
Joseph Garner <josephgarner@Josephs-MBP.modem>
2020-04-16 16:36:05 +1000

    Upload
    Added file array to submission schema
    Added Submission ID handling
    Added file handling

--- services/hack/submitHack.js
+++ services/hack/submitHack.js
@@ -2,24 +2,23 @@ import AWS from "aws-sdk";
 import { pickIfTruthy } from "../../utils";
 import { connectToDatabase } from "../database/db";
 import Submission from "../database/models/SubmissionModel";
+import mongoose from "mongoose";
 
 AWS.config.update({ region: "us-east-1" });
-const s3 = new AWS.S3({ apiVersion: "2006-03-01" });
-
-// Call S3 to list the buckets
-s3.listBuckets(function(err, data) {
-  if (err) {
-    console.log("Error", err);
-  } else {
-    console.log("Success", data.Buckets);
-  }
-});
 export const submit = async (event, context) => {
   context.callbackWaitsForEmptyEventLoop = false;
   const data = JSON.parse(event.body);
-  let { submissionId } = data;
-
+  let { submissionId, files } = data;
+  console.log(data.files);
+  console.log(data.message);
+  if(submissionId === undefined){
+    submissionId = mongoose.Types.ObjectId();
+  }
   const submit = pickIfTruthy(data, "hackId", "message");
+  submit.files = [];
+  for(let key in files){
+    submit.files.push({name: files[key].name, size: files[key].size, type: files[key].type});
+  }
   try {
     await connectToDatabase();
     await Submission.findByIdAndUpdate(submissionId, submit, {
