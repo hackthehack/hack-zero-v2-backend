f33fc93efb7eb93f0e83899286fe01ad3ef64592
Stanley <infamous_godhand@yahoo.com>
2020-04-16 11:38:39 +1000

    Making progress on mocking

--- tests/unit/getHacks.test.js
+++ tests/unit/getHacks.test.js
@@ -1,10 +1,22 @@
 import { list } from "../../services/hack/getHacks";
-jest.mock("../../services/database/db", () => {
-  return Promise.resolve({ test: "test" });
+import { connectToDatabase } from "../../services/database/db";
+import Hack from "../../services/database/models/HackModel";
+
+jest.mock("../../services/database/db", () => ({
+  __esModule: true,
+  connectToDatabase: jest.fn(),
+}));
+jest.mock("../../services/database/models/HackModel", () => {
+  return {
+    aggregate: jest.fn(),
+  };
 });
 test("END point /hacklist should return a list of hacks", async () => {
   //const spyFunc = jest.spy(list)
-
+  connectToDatabase.mockImplementation(() => () =>
+    Promise.resolve("mocked value")
+  );
+  //Hack.mockImplementation(() => () => Promise.resolve("hello"));
   const result = await list({}, {});
   console.log(result);
 });
