3a504a64a323ab3b827051579cd1a7d6aa31e3dd
Stanley <infamous_godhand@yahoo.com>
2020-04-16 11:13:53 +1000

    able to mock an imported function

--- tests/unit/getHacks.test.js
+++ tests/unit/getHacks.test.js
@@ -1,9 +1,10 @@
 import { list } from "../../services/hack/getHacks";
+jest.mock("../../services/database/db", () => {
+  return Promise.resolve({ test: "test" });
+});
+test("END point /hacklist should return a list of hacks", async () => {
+  //const spyFunc = jest.spy(list)
 
-test("END point /hacklist should return a list of hacks", () => {
-  const connectToDatabase = jest
-    .spyOn(list, "connectToDatabase")
-    .mockImplementation(() => console.log("hello world"));
-
-  expect(2 + 2).toBe(4);
+  const result = await list({}, {});
+  console.log(result);
 });
